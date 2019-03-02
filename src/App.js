604251bd986645a6828b2560f6bdb1ac8d206362
Tucker <trblackw@ncsu.edu>
2019-03-02 16:57:55 -0500

    using react.lazy and suspense to lazy load landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { Router } from "@reach/router";
-import Landing from "./Landing";
+import React, { lazy, Suspense } from 'react';
+import { Router } from '@reach/router';
+const Landing = lazy(() => import('./Landing'));
+
+const Loading = () => <div>Loading...</div>;
 
 const App = () => (
-  <Router>
-    <Landing path="/" />
-  </Router>
+	<Suspense fallback={Loading}>
+		<Router>
+			<Landing path="/" />
+		</Router>
+	</Suspense>
 );
 
 export default App;
